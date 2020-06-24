aa45ef3a8fb3b4a10b652c4fa5683dc6aa1b8d05
Aije <andryjulie@hotmail.com>
2020-06-24 15:42:50 +0200

    création liste de produit

--- listproduct.js
+++ listproduct.js
@@ -12,14 +12,14 @@ window.onload = function ()
             for (let i = 0; i < response.length; i++)
             {
                 console.log(response[i].imageUrl);
-                element.innerHTML = 
-                "<div class='row'>" +
-                    "<div class='col-12 col-lg-4'>" +
-                        "<div class='card'>" +
+                element.innerHTML +=
+                "<div class='row justify-content-center'>" +
+                    "<div class='col-12 col-lg-6'>" +
+                        "<div class='card my-5'>" +
                             "<div class='card-body'>" +
                                 "<h2 class='card-title'>"+ response[i].name +"</h2>" +
                                 "<p class='card-text'>"+ response[i].price +" Euros</p>" + 
-                                "<img width='40' height='30' alt='vintage chair'>" + response[i].image +
+                                "<img width='40' height='30' alt='vintage chair' src='"+ response[i].imageUrl + "'>" +
                                 "<a href='' class='btn btn-dark'>Voir le détail du produit 1</a>" +
                             "</div>" +
                          "</div>" +
