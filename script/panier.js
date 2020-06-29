e45e65e1c0b2233c17f369e826d2feeb40b5eef2
Aije <andryjulie@hotmail.com>
2020-06-29 18:10:19 +0200

    écriture fonction ajout panier

--- script/panier.js
+++ script/panier.js
@@ -1,16 +1,82 @@
-callApi(request, ID);
-
 window.onload = function () 
 {
-    let request = new XMLHttpRequest();
+    const REQUEST = new XMLHttpRequest();
+    const METHOD = "GET";
+    const URL = "http://localhost:3000/api/furniture/id"
 
-    request.onreadystatechange = function () 
-    {
+    REQUEST.open(METHOD, URL);
+
+    REQUEST.onreadystatechange = function (event){
         if (this.readyState == XMLHttpRequest.DONE && this.status == 200) 
         {
-            let response = JSON.parse(this.responseText);
+            let ajoutPanier = JSON.parse(this.responseText);
+            
         }
+        REQUEST.send();
     }
+
+    function ajoutPanier(imageUrl, name, price){
+        const nouvelArticlePanier = document.createElement("th");
+        nouvelArticlePanier.innerHTML = `<img>${imageUrl}</img><h3>${name}<h3/><p>${price}</p>`
+        document.querySelector("thead").appendChild(nouvelArticlePanier);
+    }
+
+    ajoutPanier
 }
 
 
+//FONCTION POUR CREER LE PANIER ?
+//<ul> dans le HTML, puis
+/*function ajoutPanier(photo, name, prix){
+    const nouvelArticlePanier = document.createElement("li");
+    nouvelArticlePanier.innerHTML = `<img>${photo}</img><h3>${name}</h3><p>${prix}</p>`;
+    document.querySelector("ul").appendChild(nouvelArticlePanier);
+}
+
+ajoutPanier("tbc", "bench", "190 euros");*/
+
+//ouverture et envoi requête GET
+/*
+const requ = new XMLHttpRequest();
+const method = "GET";
+const url = "http://localhost:3000/api/furniture/";
+
+requ.open(method, url);
+
+requ.onreadystatechange = function(event){  //récupérer la réponse readyStateChange
+    if(this.readyState === XMLHttpRequest.DONE){ //est-ce que ma requête est terminée ?
+        if(this.status === 200){  //est-ce que ma requête s'est bien passée ?(réponse 200)
+            console.log(JSON.parse(this.responseText)) //JSON.parse affiche les données sous forme d'objets
+        } else {
+            console.log("Statut" + this.status);
+        }
+    } 
+}
+requ.send();
+*/
+
+//ouverture et envoi requête POST
+/*
+const requ = new XMLHttpRequest();
+const method = "POST";
+const url = "http://localhost:3000/api/furniture/";
+
+const data = {
+    body: "blablabla",
+    title: "Titre du poste",
+    userId: 16
+};
+
+requ.open(method, url);
+
+requ.onreadystatechange = function(event){  //récupérer la réponse readyStateChange
+    if(this.readyState === XMLHttpRequest.DONE){ //est-ce que ma requête est terminée ?
+        if(this.status === 201){  //est-ce que ma requête s'est bien passée ?(réponse 201 le post est-il accepté ?)
+            console.log(JSON.parse(this.responseText)) //JSON.parse affiche les données sous forme d'objets
+        } else {
+            console.log("Statut" + this.status);
+        }
+    } 
+}
+requ.send(data);
+*/
--- script/product.js
+++ script/product.js
@@ -3,36 +3,52 @@ function callApi(request, ID) {
     request.send();
 }
 
-window.onload = function () 
-{
+//
+function ajoutPanier(id, imageUrl, name, price) {
+    let panier = JSON.parse(localStorage.getItem("panier")); //je vérifie si mon panier existe
+
+    if (panier === null) {
+        panier = [];
+    }
+
+    panier.push({
+        id: id,
+        imageUrl: imageUrl,
+        name: name,
+        price: price
+    });
+
+    localStorage.setItem("panier", JSON.stringify(panier));
+}
+
+window.onload = function () {
     let request = new XMLHttpRequest();
 
-    request.onreadystatechange = function () 
-    {
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) 
-        {
+    request.onreadystatechange = function () {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
             let response = JSON.parse(this.responseText);
+
             let element = document.getElementById("product");
-                element.innerHTML =
+            element.innerHTML =
                 "<div class='row justify-content-center'>" +
-                    "<div class='col-12 col-lg-6'>" +
-                        "<div class='card my-5'>" +
-                            "<div class='card-body'>" +
-                                "<h2 class='card-title'>"+ response.name +"</h2>" +
-                                "<p class='card-text'>"+ response.price +" Euros</p>" + 
-                                "<img width='40' height='30' alt='vintage chair' src='"+ response.imageUrl + "'>" +
-                                "<a href='../pages/panier.html' class='btn btn-dark'>Ajouter au panier</a>" +
-                            "</div>" +
-                         "</div>" +
-                    "</div>" +
+                "<div class='col-12 col-lg-6'>" +
+                "<div class='card my-5'>" +
+                "<div class='card-body'>" +
+                `<h2 class='card-title'>${response.name}</h2>` +
+                `<p class='card-text'>${response.price} Euros</p>` +
+                `<img width='40' height='30' alt='vintage chair' src="${response.imageUrl}">` +
+                `<button onclick="ajoutPanier('${response._id}','${response.imageUrl}', '${response.name}', ${response.price})" class='btn btn-dark'>Ajouter au panier</button>` +
+                "</div>" +
+                "</div>" +
+                "</div>" +
                 "</div>";
-            
+
         }
 
     }
 
     const QUERYSTRING = window.location.search;
-    
+
     const URLPARAMETERS = new URLSearchParams(QUERYSTRING);
     const ID = URLPARAMETERS.get('id');
 
