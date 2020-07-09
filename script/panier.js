8845bdf276278dc15c7cbec2e075852029ac7d14
Aije <andryjulie@hotmail.com>
2020-07-09 18:49:50 +0200

    Récupération données clients

--- script/panier.js
+++ script/panier.js
@@ -25,28 +25,28 @@ window.onload = function ()
         <form id="contact">
             <div class="row">
                 <div class="col">
-                    <input type="text" class="form-control" placeholder="Prénom">
+                    <input id="prenom" type="text" class="form-control" placeholder="Prénom">
                 </div>
                 <div class="col">
-                    <input type="text" class="form-control" placeholder="Nom">
+                    <input id="nom" type="text" class="form-control" placeholder="Nom">
                 </div>
             </div>
             <div class="row my-4">
                 <div class="col">
-                    <input type="text" class="form-control" placeholder="Adresse">
+                    <input id="adresse" type="text" class="form-control" placeholder="Adresse">
                 </div>
             </div>
             <div class="row">
                 <div class="col">
-                    <input type="text" class="form-control" placeholder="Code Postal">
+                    <input id="codepostal" type="text" class="form-control" placeholder="Code Postal">
                 </div>
                 <div class="col">
-                    <input type="text" class="form-control" placeholder="Ville">
+                    <input id="ville" type="text" class="form-control" placeholder="Ville">
                 </div>
             </div>
             <div class="row my-4">
                 <div class="col">
-                    <input type="email" class="form-control" placeholder="Adresse électronique">
+                    <input id="email" type="email" class="form-control" placeholder="Adresse électronique">
                 </div>
             </div>
             <div class="row my-4">
@@ -59,36 +59,39 @@ window.onload = function ()
 
     document.getElementById("contact").addEventListener("submit", (event) => {
         event.preventDefault();
-        console.log("yolo");
-    });
+    
 
-}
-
-
-
-
-//ouverture et envoi requête POST
-/*
-const requ = new XMLHttpRequest();
-const method = "POST";
-const url = "http://localhost:3000/api/furniture/";
-
-const data = {
-    body: "blablabla",
-    title: "Titre du poste",
-    userId: 16
-};
-
-requ.open(method, url);
-
-requ.onreadystatechange = function(event){  //récupérer la réponse readyStateChange
-    if(this.readyState === XMLHttpRequest.DONE){ //est-ce que ma requête est terminée ?
-        if(this.status === 201){  //est-ce que ma requête s'est bien passée ?(réponse 201 le post est-il accepté ?)
-            console.log(JSON.parse(this.responseText)) //JSON.parse affiche les données sous forme d'objets
-        } else {
-            console.log("Statut" + this.status);
+        const User = {
+            prenom: document.getElementById("prenom").value,
+            nom: document.getElementById("nom").value,
+            adresse: document.getElementById("adresse").value,
+            codepostal: document.getElementById("codepostal").value,
+            ville: document.getElementById("ville").value,
+            email: document.getElementById("email").value,
         }
-    } 
+        //console.log(User);
+
+        const REQUEST = new XMLHttpRequest();
+        let METHODE = "POST";
+        let URL = "http://localhost:3000/api/furniture/order";
+
+        REQUEST.open(METHODE, URL);
+
+        REQUEST.onreadystatechange = function(event){  //récupérer la réponse readyStateChange
+            if(this.readyState === XMLHttpRequest.DONE){ //est-ce que ma requête est terminée ?
+                if(this.status === 201){  //est-ce que ma requête s'est bien passée ?(réponse 201 le post est-il accepté ?)
+                    console.log(JSON.parse(this.responseText)) //JSON.parse affiche les données sous forme d'objets
+                } else {
+                    console.log("Statut" + this.status);
+                }
+            } 
+        }
+        REQUEST.send(User, panier);
+    });
+    
 }
-requ.send(data);
-*/
+
+
+
+
+
