4fabbad50fd72139e7cb0f02615bdbf07d18a0bb
Dylan <thomas.dylan191@gmail.com>
2020-07-20 15:53:34 +0200

    Fixing post request

--- script/panier.js
+++ script/panier.js
@@ -63,11 +63,10 @@ window.onload = function ()
         event.preventDefault();
     
         let contact = {
-            prenom: document.getElementById("prenom").value,
-            nom: document.getElementById("nom").value,
-            adresse: document.getElementById("adresse").value,
-            codepostal: document.getElementById("codepostal").value,
-            ville: document.getElementById("ville").value,
+            firstName: document.getElementById("prenom").value,
+            lastName: document.getElementById("nom").value,
+            address: document.getElementById("adresse").value,
+            city: document.getElementById("ville").value,
             email: document.getElementById("email").value,
         }
         //console.log(contact);
@@ -85,58 +84,52 @@ window.onload = function ()
                 nom == ""
               ) {
                 checkMessage = "Les caractères spéciaux ou les chiffres ne sont pas autorisés";
-              } else {
+            } else {
                 console.log("Nom accepté");
-              }
-              if (checkMessage != "") {
-                alert("Attention certaines données ne sont pas conformes :" + "\n" + checkMessage);
-              }
             }
+
+            if (checkMessage != "") {
+                alert("Attention certaines données ne sont pas conformes :" + "\n" + checkMessage);
+            }
+        }
           
 
         //parcourir le tableau panier et récupérer les attributs id pour en faire un tableau
-        let products = [];
-        panier.forEach(item => {
-           products.push(item.id)
-           //console.log(products);
-       });
+        const products = panier.map(item => item.id)
 
-       let send = {
-           contact, products
-       };
-       JSON.stringify(send);
-       console.log(send);
-      
-        const REQUEST = new XMLHttpRequest();
-        REQUEST.open("POST", "http://localhost:3000/api/furniture/order");
-        REQUEST.setRequestHeader("content-type", "application/JSON");
-        REQUEST.send(JSON.stringify(send));
+        const send = {
+            contact, products
+        };
         
+        console.log(JSON.stringify(send));
 
-        REQUEST.onreadystatechange = function(event){  //récupérer la réponse readyStateChange
-            if(this.readyState === XMLHttpRequest.DONE){ //est-ce que ma requête est terminée ?
-                if(this.status === 201){//est-ce que ma requête s'est bien passée ?(réponse 201 le post est-il accepté ?)
-                    let json = JSON.parse(REQUEST.responseText); 
-                    console.log(JSON.parse(this.responseText)) //JSON.parse affiche les données sous forme d'objets
-                } else {
-                    console.log(this.responseText);
-                }
-            } 
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(send),
+            headers: {
+                'Content-Type': 'application/json'
+            }
         }
-        
+
+        // send post request
+        fetch('http://localhost:3000/api/furniture/order', options)
+            .then(res => res.json())
+            .then(res => console.log(res))
+            .catch(err => console.error(err));
+                
     });
 }
 
 //retirer un item du panier
-let removeItem = 0
-removeItem.addEventListener("onclick",(event) =>{
-    panier.splice(0, 1);
-    localStorage.clear();
-    // Mise à jour du nouveau panier avec suppression de l'article
-    localStorage.setItem("panier", JSON.stringify(panier));
-    //Mise à jour de la page pour affichage de la suppression au client
-    window.location.reload();
-});  
+// let removeItem = 0
+// removeItem.addEventListener("onclick",(event) =>{
+//     panier.splice(0, 1);
+//     localStorage.clear();
+//     // Mise à jour du nouveau panier avec suppression de l'article
+//     localStorage.setItem("panier", JSON.stringify(panier));
+//     //Mise à jour de la page pour affichage de la suppression au client
+//     window.location.reload();
+// });  
 
 
        
