131114a9d5eee4922994af301642fe69d5c28959
Aije <andryjulie@hotmail.com>
2020-07-22 17:38:23 +0200

    Refactorisation fonction ajout panier suite import API

--- script/product.js
+++ script/product.js
@@ -1,60 +1,38 @@
-function callApi(request, ID) {
-    request.open("GET", "http://localhost:3000/api/furniture/" + ID);
-    request.send();
-}
-
-//Ajout Panier
-function ajoutPanier(id, imageUrl, name, price) {
-    let panier = JSON.parse(localStorage.getItem("panier"));
-    //console.log(panier);
-    if (panier === null) { //je vérifie si mon panier existe
-        panier = [];
-    }
-
-    panier.push({
-        id: id,
-        imageUrl: imageUrl,
-        name: name,
-        price: price
-    });
-
-    localStorage.setItem("panier", JSON.stringify(panier)); //les paramètres ne peuvent être que des strings donc JSON
-    afficheMessage(Message());
-}
-
-//Un message de type "alert" s'affiche à chaque article ajouté au panier
-function Message() {
-    let message1 = "Votre article a bien été ajouté au panier";
-    return message1;
- 
-}
-
-function afficheMessage(message) {
-    alert(message);
-}
+import {API_URL} from "../modules/env.mjs"
 
 function generateProduct(response){
     let element = document.getElementById("product");
-    element.innerHTML =
-        "<div class='row justify-content-center'>" +
-        "<div class='col-12 col-lg-6'>" +
-        "<div class='card my-5'>" +
-        "<div class='card-body'>" +
+
+    element.insertAdjacentHTML('afterbegin',
         `<h2 class='card-title'>${response.name}</h2>` + `<img width='100' height='100' alt='furniture' src="${response.imageUrl}">` +
         `<p class='card-text'>${response.description}</p>` +
         `<label><em>Sélectionnez votre finition</em></label>
             <select class="form-control"></select>` +
-        `<p class='card-text my-2'>${response.price} Euros</p>` +
-        `<button onclick="ajoutPanier('${response._id}','${response.imageUrl}', '${response.name}', ${response.price})" class='btn btn-dark'>Ajouter au panier</button>` +
-        "</div>" +
-        "</div>" +
-        "</div>" +
-        "</div>";
+        `<p class='card-text my-2'>${response.price} Euros</p>`);
 
     let select = document.getElementsByTagName("select")[0];
+
     response.varnish.forEach(item => {
         select.innerHTML += `<option value="${item}">${item}</option>`
     });
+
+    document.getElementById("ajout").addEventListener("click", () => {
+        let panier = JSON.parse(localStorage.getItem("panier"));
+
+        if (panier === null) { //je vérifie si mon panier existe
+            panier = [];
+        }
+
+        panier.push({
+            id: response._id,
+            imageUrl: response.imageUrl,
+            name: response.name,
+            price: response.price
+        });
+
+            localStorage.setItem("panier", JSON.stringify(panier)); //les paramètres ne peuvent être que des strings donc JSON
+            alert("Votre article a bien été ajouté au panier");
+    })
 }
 
 window.onload = function () {
@@ -72,5 +50,6 @@ window.onload = function () {
     const URLPARAMETERS = new URLSearchParams(QUERYSTRING);
     const ID = URLPARAMETERS.get('id');
 
-    callApi(request, ID);
+    request.open("GET", API_URL + ID);
+    request.send();
 }
